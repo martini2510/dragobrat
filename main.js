990dac1d1b629815740e5f4fe410d0c46c1d35f1
1 <a1@1s-iMac.lan>
2025-04-11 21:48:58 +0300

    create another paje n correct mistakes

--- main.js
+++ main.js
@@ -1,24 +1,4 @@
-
-  const modal = document.getElementById("modal");
-  const closeBtn = document.querySelector(".close-btn");
-
-  // Автоматичне відкриття через 2 секунди
-  window.addEventListener("DOMContentLoaded", () => {
-    setTimeout(() => {
-      modal.style.display = "flex";
-    }, 2000);
-  });
-
-  // Закрити модальне вікно
-  closeBtn.addEventListener("click", () => {
-    modal.style.display = "none";
-  });
-
-  // Закриття по кліку поза вікном (не обов’язково, але приємно)
-  window.addEventListener("click", (e) => {
-    if (e.target === modal) {
-      modal.style.display = "none";
-    }
-  });
-
-
+function toggleMenu() {
+  const menu = document.getElementById("mobile-menu");
+  menu.classList.toggle("open");
+}
