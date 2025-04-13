2d70018e47ad26b452c717b4b22d1efb29f8493b
1 <a1@1s-iMac.lan>
2025-04-13 12:42:27 +0300

    add new paje

---
+++ rozvagi.js
@@ -0,0 +1,25 @@
+
+
+document.addEventListener("DOMContentLoaded", () => {
+  function initCarousel(id) {
+    const carousel = document.getElementById(id);
+    if (!carousel) return;
+
+    const images = carousel.querySelectorAll("img");
+    if (images.length < 2) return;
+
+    let index = 0;
+    images[index].classList.add("active");
+
+    setInterval(() => {
+      images[index].classList.remove("active");
+      index = (index + 1) % images.length;
+      images[index].classList.add("active");
+    }, 3000);
+  }
+
+  // Запуск обох каруселей
+  initCarousel("carousel-standard");
+  initCarousel("carousel-comfort");
+});
+
